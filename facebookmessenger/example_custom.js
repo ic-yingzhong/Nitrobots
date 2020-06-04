b80ac7ad4341eca8709c7dc71ff0479f6135f577
ic-yingzhong <66402257+ic-yingzhong@users.noreply.github.com>
2020-06-04 14:28:42 +1000

    Add files via upload

---
+++ facebookmessenger/example_custom.js
@@ -0,0 +1,15 @@
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<div class="fb-customerchat"
+     page_id=" " #page_id for client’s Facebook Page
+     greeting_dialog_display="hide"	
+</div>
+<script>
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
